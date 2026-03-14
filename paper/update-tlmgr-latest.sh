772cfd29c13a0f174ff65da043a1f65f45f4051c
Natalie <nataliejakab@gmail.com>
2026-03-14 17:22:38 +0100

    latex und pdf sötted up to date si

---
+++ paper/update-tlmgr-latest.sh
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html><head>
+<title>307 Temporary Redirect</title>
+</head><body>
+<h1>Temporary Redirect</h1>
+<p>The document has moved <a href="https://mirror.foobar.to/CTAN/systems/texlive/tlnet/update-tlmgr-latest.sh">here</a>.</p>
+</body></html>
